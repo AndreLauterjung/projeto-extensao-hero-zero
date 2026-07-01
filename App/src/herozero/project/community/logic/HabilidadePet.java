a4d657dd04ad4b5bfe24e6ce00274e18bcaecc59
AndreLauterjung <andre.f.lauterjung@gmail.com>
2026-07-01 12:19:39 -0300

    refactor(HabilidadePet): atualiza mensagens erro e ajusta try-catch
    Mensagens de erro mais detalhadas quando ocorrem erros
    
    Mudança de sc.next() para sc.nextLine() no catch, para armazenar Strings com espaço

--- App/src/herozero/project/community/logic/HabilidadePet.java
+++ App/src/herozero/project/community/logic/HabilidadePet.java
@@ -2,6 +2,7 @@ package herozero.project.community.logic;
 
 import herozero.project.community.view.BonusHabilidadePetView;
 import herozero.project.community.model.FichaPersonagem;
+import herozero.project.community.view.ErrorStyleView;
 
 import java.util.Scanner;
 import java.util.InputMismatchException;
@@ -18,6 +19,7 @@ public class HabilidadePet
     public void escolhaHabilidade (Scanner sc, FichaPersonagem fichaPersonagem)
     {
         BonusHabilidadePetView perguntaHabilidadePet = new BonusHabilidadePetView();
+        ErrorStyleView errorMensagem = new ErrorStyleView();
         
         boolean isCalculandoBonusPet = true;
         int escolhaHabilidadePet = -1;
@@ -34,25 +36,20 @@ public class HabilidadePet
                 
                 if(escolhaHabilidadePet < 0 || escolhaHabilidadePet > 27)
                 {
-                    System.out.printf("\nX X X X X X X X X X X X X X X X X X X\n");
-                    System.out.println("    Você digitou um valor inválido!");
-                    System.out.println("        Tente novamente!");
-                    System.out.printf("X X X X X X X X X X X X X X X X X X X\n\n");   
+                    errorMensagem.mensagemNumeroInvalido(); 
                 }
                 else
                 {
                     isCalculandoBonusPet = false;
                     
-                } // Fim da estrutura condicional.
+                } // Fechamento da estrutura condicional.
             }
             catch(InputMismatchException e)
             {
-                System.out.printf("\nX X X X X X X X X X X X X X X X X X X X X X X X X X\n");
-                System.out.println(" ERRO: Você digitou um caractere onde deve ir número!");
-                System.out.printf("X X X X X X X X X X X X X X X X X X X X X X X X X X X \n\n");
-                sc.next();
+                errorMensagem.mensagemEntradaInvalida();
+                sc.nextLine();
             }
-        } // Fim do loop while.
+        } // Fechamento do loop while.
               
         switch(escolhaHabilidadePet)
         {
@@ -74,7 +71,6 @@ public class HabilidadePet
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(20);
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(20);
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(20);
-                
                 System.out.println("A habilidade do pet dá um bônus de 20% em todas as habilidades básicas!");
                 break;
                 
@@ -83,7 +79,6 @@ public class HabilidadePet
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(19);
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(19);
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(19);
-                
                 System.out.println("A habilidade do pet dá um bônus de 19% em todas as habilidades básicas!");
                 break;
                 
@@ -93,7 +88,6 @@ public class HabilidadePet
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(18);
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(18);
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(18);
-                
                 System.out.println("A habilidade do pet dá um bônus de 18% em todas as habilidades básicas!");
                 break;
                 
@@ -102,7 +96,6 @@ public class HabilidadePet
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(17);
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(17);
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(17);
-                
                 System.out.println("A habilidade do pet dá um bônus de 17% em todas as habilidades básicas!");
                 break;
                 
@@ -111,7 +104,6 @@ public class HabilidadePet
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(16);
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(16);
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(16);
-                
                 System.out.println("A habilidade do pet dá um bônus de 16% em todas as habilidades básicas!");
                 break;
                 
@@ -122,31 +114,26 @@ public class HabilidadePet
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(15);
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(15);
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(15);
-                
                 System.out.println("A habilidade do pet dá um bônus de 15% em todas as habilidades básicas!");
                 break;
                 
             case 14:
                 fichaPersonagem.getBonusTotal().setBonusTotalForca(10);
-                
                 System.out.println("A habilidade do pet dá um bônus de 10% na força básica!");
                 break;
                 
             case 15: 
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(25);
-                
                 System.out.println("A habilidade do pet dá um bônus de 25% no vigor básico!");
                 break;
                 
             case 16:
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(20);
-                
                 System.out.println("A habilidade do pet dá um bônus de 20% no vigor básico!");
                 break;
                 
             case 17:
                 fichaPersonagem.getBonusTotal().setBonusTotalVigor(10);
-                
                 System.out.println("A habilidade do pet dá um bônus de 10% no vigor básico!");
                 break;
                 
@@ -154,13 +141,11 @@ public class HabilidadePet
             case 19:
             case 20:
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(20);
-                
                 System.out.println("A habilidade do pet dá um bônus de 20% no cérebro básico!");
                 break;
                 
             case 21:
                 fichaPersonagem.getBonusTotal().setBonusTotalCerebro(10);
-                
                 System.out.println("A habilidade do pet dá um bônus de 10% no cérebro básico!");
                 break;
                 
@@ -169,29 +154,18 @@ public class HabilidadePet
             case 24:
             case 25:
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(25);
-                
                 System.out.println("A habilidade do pet dá um bônus de 25% na intuição básica!");
                 break;
                 
             case 26:
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(20);
-                
                 System.out.println("A habilidade do pet dá um bônus de 20% na intuição básica");
                 break;
                 
             case 27:
                 fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(10);
-                
                 System.out.println("A habilidade do pet dá um bônus de 10% na intuição básica!");
                 break;
-                
-            default:
-                fichaPersonagem.getBonusTotal().setBonusTotalForca(0);
-                fichaPersonagem.getBonusTotal().setBonusTotalVigor(0);
-                fichaPersonagem.getBonusTotal().setBonusTotalCerebro(0);
-                fichaPersonagem.getBonusTotal().setBonusTotalIntuicao(0);
-                
-                System.out.println("Valor inválido!");
                 
         } // Fechamento do switch case.
         
